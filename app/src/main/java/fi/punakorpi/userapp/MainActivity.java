032ccf73c147223f15666e3857720160719b3221
Kaisa Punakorpi <kaisa.punakorpi@gmail.com>
2024-03-15 14:56:20 +0200

    remove unused import

--- app/src/main/java/fi/punakorpi/userapp/MainActivity.java
+++ app/src/main/java/fi/punakorpi/userapp/MainActivity.java
@@ -3,7 +3,6 @@ package fi.punakorpi.userapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +11,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
